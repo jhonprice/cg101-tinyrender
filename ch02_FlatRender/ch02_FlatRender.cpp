5ad70a15b471730d1241aec66356cd5e8d448ddb
因幡组员13号 <madhatter1800@qq.com>
2022-10-04 15:59:40 +0800

    ch02

---
+++ ch02_FlatRender/ch02_FlatRender.cpp
@@ -0,0 +1,134 @@
+﻿#include "../include/ch02/tgaimage.h"
+#include "../include/ch02/model.h"
+#include "../include/ch02/geometry.h"
+#include <vector>
+#include <cmath>
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
+
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
+        steep = true;
+    }
+    if (p0.x > p1.x) {
+        std::swap(p0, p1);
+    }
+
+    for (int x = p0.x; x <= p1.x; x++) {
+        float t = (x - p0.x) / (float)(p1.x - p0.x);
+        int y = p0.y * (1. - t) + p1.y * t;
+        if (steep) {
+            image.set(y, x, color);
+        }
+        else {
+            image.set(x, y, color);
+        }
+    }
+}
+
+using Triangle = Vec2i[3];
+const float zero = 1e-9;
+
+void triangle_line(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
+
+//求重心坐标（TODO
+Vec3f barycentric(const Triangle pts, Vec2i P) {
+    Vec3f u = Vec3f(pts[2].x - pts[0].x,
+        pts[1].x - pts[0].x,
+        pts[0].x - P.x) ^
+        Vec3f(pts[2].y - pts[0].y,
+            pts[1].y - pts[0].y,
+            pts[0].y - P.y);
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(const Triangle pts, TGAImage& image, TGAColor color) {
+    //构建包围盒
+    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+    for (int i = 0; i < 3; i++) {
+        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    }
+    //光栅化
+    Vec2i P;
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+            Vec3f bc_screen = barycentric(pts, P);
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+            image.set(P.x, P.y, color);
+        }
+    }
+}
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+    if (t0.y == t1.y && t0.y == t2.y) return; // i dont care about degenerate triangles
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++) {
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y
+        }
+    }
+}
+
+
+int main(int argc, char** argv) {
+    if (2 == argc) {
+        model = new Model(argv[1]);
+    }
+    else {
+        model = new Model("../obj/ch02/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+    Vec3f light_dir(0, 0, -1);
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j = 0; j < 3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            world_coords[j] = v;
+        }
+        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        n.normalize();
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            triangle(screen_coords, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        }
+    }
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output1.tga");
+    delete model;
+    return 0;
+}
--- ch02_Rasterization/ch02_Rasterization.cpp
+++ ch02_Rasterization/ch02_Rasterization.cpp
@@ -2,19 +2,109 @@
 //
 
 #include <iostream>
+#include "../include/ch02/tgaimage.h"
+#include "../include/ch02/geometry.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
+
+const int width = 200;
+const int height = 200;
+
+
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
+	bool steep = false;
+	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) { // if the line is steep, we transpose the image 
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
+		steep = true;
+	}
+	if (p0.x > p1.x) { // make it left−to−right 
+		std::swap(p0, p1);
+	}
+	for (int x{ p0.x }; x <= p1.x; x++) {
+		float t = (x - p0.x) / (float)(p1.x - p0.x);
+		int y = p0.y * (1. - t) + p1.y * t;
+		if (steep) {
+			image.set(y, x, color); // if transposed, de−transpose 
+		}
+		else {
+			image.set(x, y, color);
+		}
+	}
+}
+
+
+using Triangle = Vec2i[3];
+const float zero = 1e-9;
+
+void triangle_line(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+    line(t0, t1, image, color);
+    line(t1, t2, image, color);
+    line(t2, t0, image, color);
+}
+
+//求重心坐标（TODO
+Vec3f barycentric(const Triangle pts, Vec2i P) {
+	Vec3f u = Vec3f(pts[2].x - pts[0].x, 
+		pts[1].x - pts[0].x, 
+		pts[0].x - P.x) ^ 
+		Vec3f(pts[2].y - pts[0].y, 
+			pts[1].y - pts[0].y, 
+			pts[0].y - P.y);
+	/* `pts` and `P` has integer value as coordinates
+	   so `abs(u[2])` < 1 means `u[2]` is 0, that means
+	   triangle is degenerate, in this case return something with negative coordinates */
+	if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(const Triangle pts, TGAImage& image, TGAColor color) {
+	//构建包围盒
+	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bboxmax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++) {
+		bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+		bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+	}
+	//光栅化
+	Vec2i P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+			Vec3f bc_screen = barycentric(pts, P);
+			if (bc_screen.x <= zero || bc_screen.y <= zero || bc_screen.z <= zero) continue;
+			image.set(P.x, P.y, color);
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    TGAImage image(width, height, TGAImage::RGB);
+
+	Triangle t0 = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+	Triangle t1 = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+	Triangle t2 = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+
+	triangle_line(t0[0], t0[1], t0[2], image, white);
+	triangle_line(t1[0], t1[1], t1[2], image, white);
+	triangle_line(t2[0], t2[1], t2[2], image, white);
+
+	triangle(t0, image, red);
+	triangle(t1, image, green);
+	triangle(t2, image, blue);
+
+
+    image.flip_vertically();
+    image.write_tga_file("output1.tga");
+
+
+    return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
